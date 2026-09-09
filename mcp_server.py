f0042e09fea7092fabde2fe0d8714d4cc991b493
AjinkyaHimanshu <ajinkyahimanshu007@gmail.com>
2026-09-09 11:35:54 +0530

    Updating the MCP server codes

---
+++ mcp_server.py
@@ -0,0 +1,51 @@
+from mcp.server.fastmcp import FastMCP
+from pydantic import Field
+
+mcp = FastMCP("DocumentMCP", log_level="ERROR")
+
+
+docs = {
+    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E.",
+    "report.pdf": "The report details the state of a 20m condenser tower.",
+    "financials.docx": "These financials outline the project's budget and expenditures.",
+    "outlook.pdf": "This document presents the projected future performance of the system.",
+    "plan.md": "The plan outlines the steps for the project's implementation.",
+    "spec.txt": "These specifications define the technical requirements for the equipment.",
+}
+
+@mcp.tool(
+    name="read_doc",
+    description="Read the contents of a document."
+)
+
+def read_document(
+    doc_id: str = Field(description="The ID of the document to read.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    
+    return docs.get(doc_id)
+
+# TODO: Write a tool to edit a doc
+@mcp.tool(
+    name="edit_doc",
+    description="Edit the contents of a document by replacing the old content with new content."
+)
+def edit_document(
+    doc_id: str = Field(description="The ID of the document to edit."),
+    old_string: str = Field(description="The old string of the document, must match including whitespace and punctuation."),
+    new_string: str = Field(description="The new content for the document.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    
+    docs[doc_id] = docs[doc_id].replace(old_string, new_string)
+
+# TODO: Write a resource to return all doc id's
+# TODO: Write a resource to return the contents of a particular doc
+# TODO: Write a prompt to rewrite a doc in markdown format
+# TODO: Write a prompt to summarize a doc
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
